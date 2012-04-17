0288c6f76405aef14d9df64848d3c57cdbe06ee5
saurabh kumar <saurabh.nitc10@gmail.com>
2012-04-18 03:10:20 +0530

    news class created

---
+++ model/class.news.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* news class is a  class for various type of news class funtions.
+* @author Saurabh kumar <saurabh.nitc10@gmail.com>
+* @copyright Copyright (c) 2012, Saurabh kumar
+* @license http://www.gnu.org/licenses/gpl.html GNU General Public License 
+* @package news
+*/
+
+?>
